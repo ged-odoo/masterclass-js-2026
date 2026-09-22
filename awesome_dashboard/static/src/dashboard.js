8d25181027e4fb22169d03aefecb884658a44590
Géry Debongnie <ged@odoo.com>
2026-09-22 15:42:35 +0200

    wip

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,14 +1,27 @@
-import { Component, usePlugin } from "@odoo/owl";
+import { Component, usePlugin, onWillStart, signal, onMounted } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
 import { ActionPlugin } from "@web/webclient/actions/action_plugin";
+import { DashboardItem } from "./dashboard_item";
+import { rpc } from "@web/core/network/rpc";
+import { ReportingPlugin } from "./reporting_plugin";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
-    static components = { Layout };
+    static components = { Layout, DashboardItem };
     action = usePlugin(ActionPlugin);
+    statistics = signal(null);
+    reporting = usePlugin(ReportingPlugin);
+
+    setup() {
+        onWillStart(async () => {
+            const data = await this.reporting.loadStatistics();
+            this.statistics.set(data);
+        });
+    }
 
     openCustomerView() {
+        console.log("Opening customer view...");
         this.action.doAction("base.action_partner_form");
     }
 
